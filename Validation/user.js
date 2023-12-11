bafd756d0828dfdae5e166348339dd6f388b896c
PDcuong22 <henrywavip@gmail.com>
2023-12-11 23:26:34 +0700

    fix port

---
+++ Validation/user.js
@@ -0,0 +1,28 @@
+import Joi from "joi";
+
+export const userValidator = Joi.object({
+    userName: Joi.string().required.min(6).max(255).messages({
+        "string.empty": "useName không được để trống!",
+        "any.required": "userName là bắt buộc!",
+        "string.min": "userName phải có ít nhất {#litmit} ký tự",
+        "string.max": "userName phải có ít hơn {#litmit + 1} ký tự",
+    }),
+    email: Joi.string().required().email().messages({
+        "string.empty": "Email không được để trống!",
+        "any.required": "Email là bắt buộc!",
+        "string.email": "Email không đúng định dạng"
+    }),
+    password: Joi.string().required().min(6).max(255).messages({
+        "string.empty": "Password không được để trống!",
+        "any.required": "Password là bắt buộc!",
+        "string.min": "Password phải có ít nhất {#litmit} ký tự",
+        "string.max": "Password phải có ít hơn {#litmit + 1} ký tự",
+    }),
+    confirmPassword: Joi.string().required().min(6).max(255).valid(Joi.ref("password")).messages({
+        "string.empty": "confirmPassword không được để trống!",
+        "any.required": "confirmPassword là bắt buộc!",
+        "string.min": "confirmPassword phải có ít nhất {#litmit} ký tự",
+        "string.max": "confirmPassword phải có ít hơn {#litmit + 1} ký tự",
+        "any.only": "confirmPassword không khớp với password"
+    })
+})
